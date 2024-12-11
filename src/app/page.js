242b3bf256a4ead307210555a853a8e85d94df50
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2024-12-11 15:02:26 +0700

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}) {
   const {genre} = await searchParams || 'fetchTrending';
   const res = await fetch(
-    `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
+    {next: {revalidate: 3600}}
   );
   const data = await res.json();
   if(!res.ok) {
